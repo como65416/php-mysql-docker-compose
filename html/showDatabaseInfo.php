ffd94f555d5b84f870150ea282569559fbaee11d
comoco <como65416@gmail.com>
2019-02-09 23:31:12 +0800

    add php demo file

---
+++ html/showDatabaseInfo.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+// 設定DB
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'mysql57',
+    'database'  => '',
+    'username'  => 'root',
+    'password'  => 'abcd1234',
+    'charset'   => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$conn = $capsule->getConnection();
+$infos = $conn->select('show databases;');
+print_r($infos);
---
+++ html/showDomainIp.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "phpfpm IP : " . gethostbyname("phpfpm") . "<br />";
+echo "mysql IP : " . gethostbyname("mysql57") . "<br />";
+echo "web IP : " . gethostbyname("web") . "<br />";
